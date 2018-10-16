3d2d1970caf08a7ae267a79b4dc31a0a77833e0d
Peng Zhang <wrzxzp215@163.com>
2018-10-16 16:34:44 +0800

    for dialog

--- packages/dialog/src/component.vue
+++ packages/dialog/src/component.vue
@@ -133,11 +133,11 @@
     computed: {
       style() {
         let style = {};
+        if (this.width) {
+          style.width = this.width;
+        }
         if (!this.fullscreen) {
           style.marginTop = this.top;
-          if (this.width) {
-            style.width = this.width;
-          }
         }
         return style;
       }
